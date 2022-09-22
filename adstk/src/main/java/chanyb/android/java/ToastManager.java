eb05249e030565f418f5fe337cf0bf877db413e9
unknown <chanyb0827@gmail.com>
2022-09-22 17:06:23 +0900

    add: (toastManager) init

---
+++ adstk/src/main/java/chanyb/android/java/ToastManager.java
@@ -0,0 +1,31 @@
+package chanyb.android.java;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+public class ToastManager {
+    private static ToastManager instance = new ToastManager();
+    private Toast toast;
+
+    private ToastManager () {}
+
+    public static ToastManager getInstance() {
+        return instance;
+    }
+
+    public void showToast(Context context, String msg) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }, 0);
+
+    }
+
+
+}
